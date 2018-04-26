938095d6b401d2e7d3e418cb98cda14359666014
CreeperLin <965936317@qq.com>
2018-04-26 19:47:05 +0800

    test test

--- build.bash
+++ build.bash
@@ -2,4 +2,4 @@ set -e
 cd "$(dirname "$0")"
 mkdir -p bin
 i=`find ./demo/Mac/antlr4-cpp-demo/ ./demo/Mac/AST ./demo/generated -name "*.cpp"`
-g++-5 -O2 -I ./runtime/src -I ./runtime/src/misc -I ./runtime/src/atn -I ./runtime/src/dfa -I ./runtime/src/tree -I ./runtime/src/support -I ./demo/generated -I ./demo/Mac/AST -L./lib -lantlr4-runtime -std=c++11 -o ./bin/compiler $i
+g++-5 -I ./runtime/src -I ./runtime/src/misc -I ./runtime/src/atn -I ./runtime/src/dfa -I ./runtime/src/tree -I ./runtime/src/support -I ./demo/generated -I ./demo/Mac/AST -L./lib -lantlr4-runtime -std=c++11 -o ./bin/compiler $i
