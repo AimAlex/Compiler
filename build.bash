bb8d704994557065bdf9af5ad4eefc0a6962276c
AimAlex <alex-li@sjtu.edu.cn>
2018-04-26 17:09:59 +0800

    faster

--- build.bash
+++ build.bash
@@ -1,5 +1,5 @@
 set -e
 cd "$(dirname "$0")"
 mkdir -p bin 
-i=`find ./demo/Mac/antlr4-cpp-demo/ ./demo/Mac/AST ./demo/generated ./runtime -name "*.cpp"`
-g++ -I ./runtime/src -I ./runtime/src/misc -I ./runtime/src/atn -I ./runtime/src/dfa -I ./runtime/src/tree -I ./runtime/src/support -I ./demo/generated -I ./demo/Mac/AST -std=c++14 -o ./bin/compiler $i
+i=`find ./demo/Mac/antlr4-cpp-demo/ ./demo/Mac/AST ./demo/generated -name "*.cpp"`
+g++ -I ./runtime/src -I ./runtime/src/misc -I ./runtime/src/atn -I ./runtime/src/dfa -I ./runtime/src/tree -I ./runtime/src/support -I ./demo/generated -I ./demo/Mac/AST -L ./lib -l antlr4-runtime -std=c++14 -o ./bin/compiler $i
