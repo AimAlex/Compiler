cd1378e14b2f56825c683129346cb6abd940fd31
AimAlex <alex-li@sjtu.edu.cn>
2018-04-26 21:21:08 +0800

    g++7.3

--- lib.bash
+++ lib.bash
@@ -2,4 +2,4 @@ set -e
 cd "$(dirname "$0")"
 mkdir -p bin 
 i=`find ./runtime/ -name "*.cpp"`
-g++-5 -I ./runtime/src -I ./runtime/src/misc -I ./runtime/src/atn -I ./runtime/src/dfa -I ./runtime/src/tree -I ./runtime/src/support -fPIC -shared -std=c++11 -o ./lib/libantlr4-runtime.so $i
+g++-7 -I ./runtime/src -I ./runtime/src/misc -I ./runtime/src/atn -I ./runtime/src/dfa -I ./runtime/src/tree -I ./runtime/src/support -fPIC -shared -std=c++11 -o ./lib/libantlr4-runtime.so $i
