5a84bf4017db365492b87629a69a0f7559d60e97
AimAlex <alex-li@sjtu.edu.cn>
2018-04-26 16:30:02 +0800

    hh

--- build.bash
+++ build.bash
@@ -1,4 +1,5 @@
 set -e
 cd "$(dirname "$0")"
 mkdir -p bin 
-find ./demo/Mac/antlr4-cpp-demo/ ./demo/Mac/AST ./demo/generated ./runtime -name "*.cpp" | g++ -I ./runtime/src -I ./runtime/src/misc -I ./runtime/src/atn -I ./runtime/src/dfa -I ./runtime/src/tree -I ./runtime/src/support -I ./demo/generated -I ./demo/Mac/AST -std=c++14 -o ./bin/compiler @/dev/stdin
+i=`find ./demo/Mac/antlr4-cpp-demo/ ./demo/Mac/AST ./demo/generated ./runtime -name "*.cpp"`
+g++ -I ./runtime/src -I ./runtime/src/misc -I ./runtime/src/atn -I ./runtime/src/dfa -I ./runtime/src/tree -I ./runtime/src/support -I ./demo/generated -I ./demo/Mac/AST -std=c++14 -o ./bin/compiler $i
