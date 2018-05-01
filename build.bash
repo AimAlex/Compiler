f89def42fbd9b3130e5cc0a89cfb7c0d9ee5a94d
AimAlex <alex-li@sjtu.edu.cn>
2018-05-01 09:10:17 +0800

    test

--- build.bash
+++ build.bash
@@ -1,5 +1,5 @@
 set -e
 cd "$(dirname "$0")"
 mkdir -p bin
-i=`find ./demo/Mac/antlr4-cpp-demo/ ./demo/Mac/AST ./demo/Mac/VisitAST ./demo/generated -name "*.cpp"`
-g++-7 -I ./runtime/src -I ./runtime/src/misc -I ./runtime/src/atn -I ./runtime/src/dfa -I ./runtime/src/tree -I ./runtime/src/support -I ./demo/generated -I ./demo/Mac/AST -I ./demo/Mac/VisitAST -I ./demo/Mac/Symbol -L./lib -lantlr4-runtime -std=c++11 -o ./bin/compiler $i
+i=`find ./demo/Mac/antlr4-cpp-demo/ ./demo/Mac/AST ./demo/Mac/VisitAST ./demo/generated  ./runtime -name "*.cpp"`
+g++-7 -I ./runtime/src -I ./runtime/src/misc -I ./runtime/src/atn -I ./runtime/src/dfa -I ./runtime/src/tree -I ./runtime/src/support -I ./demo/generated -I ./demo/Mac/AST -I ./demo/Mac/VisitAST -I ./demo/Mac/Symbol -I ./runtime/src -I ./runtime/src/misc -I ./runtime/src/atn -I ./runtime/src/dfa -I ./runtime/src/tree -I ./runtime/src/support -std=c++11 -o ./bin/compiler $i
