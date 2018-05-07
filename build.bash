58e8ea08bc15d4a50d957cbe1e97506af8f17424
AimAlex <alex-li@sjtu.edu.cn>
2018-05-07 12:01:54 +0800

    passpasspass

--- build.bash
+++ build.bash
@@ -2,4 +2,4 @@ set -e
 cd "$(dirname "$0")"
 mkdir -p bin
 i=`find ./demo/Mac/antlr4-cpp-demo/ ./demo/Mac/AST ./demo/Mac/VisitAST ./demo/generated -name "*.cpp"`
-g++-7 -I ./runtime/src -I ./runtime/src/misc -I ./runtime/src/atn -I ./runtime/src/dfa -I ./runtime/src/tree -I ./runtime/src/support -I ./demo/generated -I ./demo/Mac/AST -I ./demo/Mac/VisitAST -I ./demo/Mac/Symbol -L ./lib -l antlr4-runtime -std=c++11 -o ./bin/compiler $i
+g++-7 -I ./runtime/src -I ./runtime/src/misc -I ./runtime/src/atn -I ./runtime/src/dfa -I ./runtime/src/tree -I ./runtime/src/support -I ./demo/generated -I ./demo/Mac/AST -I ./demo/Mac/VisitAST -I ./demo/Mac/Symbol  -std=c++11 -o ./bin/compiler $i ./lib/libantlr4-runtime.a
--- semantic.bash
+++ semantic.bash
@@ -1,3 +1,2 @@
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./lib
 cat > ./test.mx
 ./bin/compiler
