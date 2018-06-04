70938f292bc94e7744115874a89addff175d4333
AimAlex <alex-li@sjtu.edu.cn>
2018-06-04 16:02:11 +0800

    failed

--- build.bash
+++ build.bash
@@ -1,5 +1,5 @@
 set -e
 cd "$(dirname "$0")"
 mkdir -p bin
-i=`find ./demo/Mac/antlr4-cpp-demo/ ./demo/Mac/AST ./demo/Mac/VisitAST ./demo/generated -name "*.cpp"`
-g++-7 -I ./runtime/src -I ./runtime/src/misc -I ./runtime/src/atn -I ./runtime/src/dfa -I ./runtime/src/tree -I ./runtime/src/support -I ./demo/generated -I ./demo/Mac/AST -I ./demo/Mac/VisitAST -I ./demo/Mac/Symbol  -std=c++11 -o ./bin/compiler $i ./lib/libantlr4-runtime.a
+i=`find ./demo/Mac/antlr4-cpp-demo/ ./demo/Mac/AST ./demo/Mac/VisitAST ./demo/Mac/VisitIR ./demo/Mac/IR ./demo/generated -name "*.cpp"`
+g++-7 -I ./runtime/src -I ./runtime/src/misc -I ./runtime/src/atn -I ./runtime/src/dfa -I ./runtime/src/tree -I ./runtime/src/support -I ./demo/generated -I ./demo/Mac/AST -I ./demo/Mac/VisitAST -I ./demo/Mac/Symbol ./demo/Mac/VisitIR ./demo/Mac/IR -std=c++11 -o ./bin/compiler $i ./lib/libantlr4-runtime.a
